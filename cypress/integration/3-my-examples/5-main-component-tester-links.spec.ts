0b6e6c7a4eb283591c8e18c8cd8f9e76bd8e56c0
Stefan Spec <stefan@sportlogiq.com>
2022-02-02 08:16:08 -0500

    adding tests and template tests for Malcolm

---
+++ cypress/integration/3-my-examples/5-main-component-tester-links.spec.ts
@@ -0,0 +1,18 @@
+describe('Main page tester links', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+    })
+
+    it('blue test page link should go to blue page', () => {
+        cy.get('[data-cy="blue-button"]').click();
+        cy.url().should('include', 'color=blue');
+        cy.url().should('eq', 'http://localhost:4200/tests?color=blue');
+    })
+
+    it('red test page link should go to red page', () => {
+        cy.get('[data-cy="red-button"]').click();
+        cy.url().should('include', 'color=red');
+        cy.url().should('eq', 'http://localhost:4200/tests?color=red');
+    })
+})
---
+++ cypress/integration/3-my-examples/5-main-component.tester-links-malcolm.spec.ts
@@ -0,0 +1,21 @@
+describe('Main page tester links Malcolm version', () => {
+
+    /**
+     * there are two links on the main page of the app
+     * one goes to a blue version of the tests page
+     * and the other goes to a red version
+     * I want to verify:
+     * 
+     * 1. That when I click on each button it goes to the correct page
+     * 2. That the query params are correct for each color
+     */
+
+
+    it('blue test page link should go to blue page with correct params', () => {
+        
+    })
+
+    it('red test page link should go to red page with correct params', () => {
+        
+    })
+})
