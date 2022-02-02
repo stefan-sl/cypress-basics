080e78faaf4f1796faeb40297dc343e8d7952e1b
Stefan Spec <stefan@sportlogiq.com>
2022-02-02 08:08:28 -0500

    adding color properties to tester page

--- src/app/main/tester/tester.component.ts
+++ src/app/main/tester/tester.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { filter, map, Observable, shareReplay, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-tester',
   templateUrl: './tester.component.html',
   styleUrls: ['./tester.component.scss']
 })
-export class TesterComponent implements OnInit {
+export class TesterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public class$: Observable<string>;
 
-  ngOnInit(): void {
+  private _destroy$: Subject<boolean> = new Subject();
+
+  constructor(
+    private _route: ActivatedRoute
+  ) { }
+
+  public ngOnInit(): void {
     console.log('Tester component');
+
+    this._route.queryParams.pipe(
+      filter(p => !!p),
+      takeUntil(this._destroy$)
+    ).subscribe(params => {
+      console.log('Params', params);
+    })
+
+    this.class$ = this._route.queryParams.pipe(
+      filter(p => !!p),
+      map(p => {
+        return p['color'];
+      }),
+      shareReplay(1)
+    );
+  }
+
+  public ngOnDestroy(): void {
+      this._destroy$.next(true);
   }
 
 }
